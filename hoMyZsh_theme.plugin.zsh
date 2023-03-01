be746963be1638dc7bc85cf96f87a253f2a155d1
Mboukhal <85312140+Mboukhal@users.noreply.github.com>
2023-03-01 18:27:26 +0100

    Create hoMyZsh_theme.plugin.zsh

---
+++ hoMyZsh_theme.plugin.zsh
@@ -0,0 +1,1 @@
+PS1='%{$fg[green]%}[$?]%{$reset_color%} %{$fg[cyan]%}[$PWD]%{$reset_color%} $(git_prompt_info) %{$fg[red]%}[uname -a | tr " " "\n" | grep "e*r*p" | tr "." "\n" | grep "e*r*p"]%{$reset_color%} %{$fg[yellow]%}$(printf "\n ➜ ") %{$reset_color%}'
